da0dc38b0cca8f1b3c1ed258428fa7f98b616f61
ksgadiya1 <kamalgadiya2603@gmail.com>
2025-08-04 20:44:57 +0530

    array from permutation

--- ArrayFromPermutation.java
+++ ArrayFromPermutation.java
@@ -1,4 +1,15 @@
-public class ArrayFromPermutation {
-    public static void main(String[] args) {
-        int[] arr = {1, 2, 3, 4, 5}
+
+
+
+public class ArrayFromPermutation{
+    public int[] buildArray(int[] nums){
+        int n= nums.length;
+        int[] ans = new int[n];
+
+        for (int i=0;i<n;i++){
+            ans[i]=nums[nums[i]];
+        }
+
+        return ans;
+    }
 }
